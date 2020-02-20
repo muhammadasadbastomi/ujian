9b5b74a944467a6824f1e00b84fca983add156ce
muhammadasadbastomi <tomycules@gmail.com>
2020-02-20 14:44:27 +0800

    setup route ujian

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/','UjianController@ujian_index')
+->name('ujian_index');
+Route::post('/ujian/tambah','UjianController@createIndex')
+->name('ujian_tambah_index');
+Route::post('/ujian/tambah','UjianController@create')
+->name('ujian_tambah');
+Route::get('/ujian/edit/{id}','UjianController@find')
+->name('ujian_edit');
+Route::put('/ujian/edit/{id}','UjianController@ujian_update')
+->name('ujian_update');
+Route::get('/ujian/hapus/{id}','UjianController@ujian_hapus')
+->name('ujian_hapus');
